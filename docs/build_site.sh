6757e238b392658de6f3a5b22031ee5df8db0617
caufieldjh <j.harry.caufield@gmail.com>
2022-09-27 14:31:25 -0400

    Have build_site retrieve onto status

--- docs/build_site.sh
+++ docs/build_site.sh
@@ -4,7 +4,11 @@
 # Define paths
 JEKYLL_CONFIG_HEADER_FILE="_config_header.yml"
 JEKYLL_CONFIG_FILE="_config.yml"
+ONTO_STATUS_URL="https://kg-hub.berkeleybop.io/kg-bioportal/onto_status.yaml"
 ONTO_STATUS_FILE="onto_status.yaml"
+
+# Retrieve most recent KG-Bioportal ontology status
+wget -N $ONTO_STATUS_URL
 
 # Append ontology status list
 echo "Adding all lists to Jekyll config."
