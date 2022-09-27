20f21352ee26c9a49f6219bd3fa733946c6acebe
caufieldjh <j.harry.caufield@gmail.com>
2022-09-27 15:03:59 -0400

    Retrieve graph stats, too

--- docs/build_site.sh
+++ docs/build_site.sh
@@ -4,12 +4,17 @@
 # Define paths
 JEKYLL_CONFIG_HEADER_FILE="_config_header.yml"
 JEKYLL_CONFIG_FILE="_config.yml"
+GRAPH_STATS_URL="https://kg-hub.berkeleybop.io/kg-bioportal/graph_stats.yaml"
+GRAPH_STATS_FILE="graph_stats.yaml"
 ONTO_STATUS_URL="https://kg-hub.berkeleybop.io/kg-bioportal/onto_status.yaml"
 ONTO_STATUS_FILE="onto_status.yaml"
+
+# Retrieve most recent KG-Bioportal general stats
+wget -N $GRAPH_STATS_URL
 
 # Retrieve most recent KG-Bioportal ontology status
 wget -N $ONTO_STATUS_URL
 
 # Append ontology status list
 echo "Adding all lists to Jekyll config."
-cat $JEKYLL_CONFIG_HEADER_FILE $ONTO_STATUS_FILE > $JEKYLL_CONFIG_FILE
+cat $JEKYLL_CONFIG_HEADER_FILE $GRAPH_STATS_FILE $ONTO_STATUS_FILE > $JEKYLL_CONFIG_FILE
