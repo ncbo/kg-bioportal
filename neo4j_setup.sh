2398864e8bfb8761c0bb3d33898fa43b997beed3
caufieldjh <j.harry.caufield@gmail.com>
2022-12-15 15:52:31 -0500

    Expand docker container checks

--- neo4j_setup.sh
+++ neo4j_setup.sh
@@ -4,7 +4,21 @@
 
 GRAPH_PATH="./data/merged/"
 TX_CONFIG_PATH="neo4j-transform.yaml"
+CONTAINER_NAME="kgx-neo4j"
 
 # Run
+echo "Set up the Neo4j Docker container..."
+if [ ! "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    # If container isn't running, run that container
+    # This will download if needed
+    docker run -d --rm --name $CONTAINER_NAME \
+            -p 7474:7474 -p 7687:7687 \
+            --env NEO4J_AUTH=neo4j/test \
+            neo4j:4.3
+else
+    echo "Container is already running."
+    docker ps -f name=$CONTAINER_NAME
+fi
+
 echo "Transforming KG-Bioportal to Neo4j..."
-kgx transform --transform-config $TX_CONFIG_PATH
+#kgx transform --transform-config $TX_CONFIG_PATH
