be50e3da2bc99cca4655e670fcc893416b8e2c3a
caufieldjh <j.harry.caufield@gmail.com>
2024-08-15 16:08:56 -0400

    Init downloader class properly

--- src/kg_bioportal/downloader.py
+++ src/kg_bioportal/downloader.py
@@ -2,30 +2,53 @@
 
 import logging
 
+
 class Downloader:
 
     # Directory to save the downloaded files
     output_dir: str = "data/raw"
-    
+
     # If True, downloads only the first 5 kB of each (uncompressed) source,
     # for testing and file checks
     snippet_only: bool = False
-    
+
     # If True, ignore cache and download files even if they exist
     ignore_cache: bool = False
 
-    def download(self, onto_list: list = []) -> None:
-        """Downloads data files from list of ontologies into data directory.
+    def __init__(
+        self,
+        output_dir: str = "data/raw",
+        snippet_only: bool = False,
+        ignore_cache: bool = False,
+    ) -> None:
+        """Initializes the Downloader class.
 
         Args:
             output_dir: A string pointing to the location to download data to.
-            snippet_only: Downloads only the first 5 kB of the source, for testing and file checks. 
+            snippet_only: Downloads only the first 5 kB of the source, for testing and file checks.
             ignore_cache: Ignore cache and download files even if they exist
 
         Returns:
             None.
         """
+        self.output_dir = output_dir
+        self.snippet_only = snippet_only
+        self.ignore_cache = ignore_cache
 
-        print("TEST")
+        return None
+
+    def download(self, onto_list: list = []) -> None:
+        """Downloads data files from list of ontologies into data directory.
+
+        Args:
+
+            onto_list: A list of ontologies to download by name. 
+            Names should be those used in BioPortal, e.g., PO, SEPIO, etc.
+
+        Returns:
+            None.
+        """
+        for ontology in onto_list:
+            logging.info(f"Downloading {ontology}...")
 
         return None
