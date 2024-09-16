de4c1ad531bbc91444edbfa92693273a74e6410f
caufieldjh <j.harry.caufield@gmail.com>
2024-09-16 17:43:21 -0400

    Change viz to bar plots

--- docs/make_viz.py
+++ docs/make_viz.py
@@ -14,36 +14,40 @@ with open("onto_status.yaml", "r") as infile:
 
 # Node counts across all ontologies, unmerged
 nodeontos = ontos.loc[ontos["nodecount"] < 150000, "id"] = "All other ontologies"
-fig1 = px.pie(
+fig1 = px.bar(
     ontos,
-    values="nodecount",
-    names="id",
-    title="Nodes used to make KG-Bioportal",
-    hole=0.3,
+    x="id",
+    y="nodecount",
+    title="Nodes in KG-Bioportal",
+    text="nodecount"
 )
-fig1.update_traces(textposition="inside", textinfo="percent+label")
+fig1.update_traces(texttemplate='%{text:.2s}', textposition='outside')
 fig1.update_layout(
     uniformtext_minsize=14,
     uniformtext_mode="hide",
     plot_bgcolor="rgba(0, 0, 0, 0)",
     paper_bgcolor="rgba(0, 0, 0, 0)",
+    xaxis_title="Ontology ID",
+    yaxis_title="Node Count"
 )
 fig1.write_html("_includes/fig1.html", include_plotlyjs="cdn")
 
 # Edge counts across all ontologies, unmerged
 ontos.loc[ontos["edgecount"] < 150000, "id"] = "All other ontologies"
-fig2 = px.pie(
+fig2 = px.bar(
     ontos,
-    values="edgecount",
-    names="id",
-    title="Edges used to make KG-Bioportal",
-    hole=0.3,
+    x="id",
+    y="edgecount",
+    title="Edges in KG-Bioportal",
+    text="edgecount"
 )
-fig2.update_traces(textposition="inside", textinfo="percent+label")
+fig2.update_traces(texttemplate='%{text:.2s}', textposition='outside')
 fig2.update_layout(
     uniformtext_minsize=14,
     uniformtext_mode="hide",
     plot_bgcolor="rgba(0, 0, 0, 0)",
     paper_bgcolor="rgba(0, 0, 0, 0)",
+    xaxis_title="Ontology ID",
+    yaxis_title="Edge Count"
 )
 fig2.write_html("_includes/fig2.html", include_plotlyjs="cdn")
